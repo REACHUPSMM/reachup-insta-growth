c1804e3ef578399e9e1b50eabb2dd06839609dbc
REACHUPSMM <reachupsmm@gmail.com>
2026-03-23 22:21:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,34 +1,33 @@
 function openPage(page){
-  document.querySelector(".steps").style.display = "none";
-  document.querySelector(".categories").style.display = "none";
+  document.querySelector(".center").style.display="none";
+  document.querySelector(".steps").style.display="none";
 
   document.getElementById(page).classList.remove("hidden");
 }
 
-let selectedPlan = "";
+let selectedPlan="";
 
 function buy(plan){
-  selectedPlan = plan;
+  selectedPlan=plan;
   document.getElementById("payment").classList.remove("hidden");
-  window.scrollTo(0, document.body.scrollHeight);
 }
 
 function done(){
-  let username = document.getElementById("insta").value;
+  let username=document.getElementById("insta").value;
 
   if(username===""){
     alert("Enter Instagram ID");
     return;
   }
 
-  let phone = "917695990053";
-  let msg = `Hi, Payment done ✅\nPlan: ${selectedPlan}\nID: ${username}`;
+  let phone="917695990053";
+  let msg=`Hi, Payment done ✅\nPlan: ${selectedPlan}\nID: ${username}`;
 
-  window.location.href = `https://wa.me/${phone}?text=${encodeURIComponent(msg)}`;
+  window.location.href=`https://wa.me/${phone}?text=${encodeURIComponent(msg)}`;
 }
 
-// LIVE COUNT
+/* LIVE COUNT */
 setInterval(()=>{
-  let num = Math.floor(Math.random()*200)+100;
-  document.getElementById("liveCount").innerText = num;
+  let num=Math.floor(Math.random()*200)+100;
+  document.getElementById("liveCount").innerText=num;
 },2000);
